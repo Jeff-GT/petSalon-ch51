0b51fd1151963d19efcfd969313ddf96a9ac479f
Jeff-GT <jefferson.guerrero@sdgku.edu>
2024-09-06 16:06:48 -0700

    2nd Commit - Assignemt #1 -

--- scripts/register.js
+++ scripts/register.js
@@ -6,29 +6,63 @@ let pet1={
     name:"Winter",
     age:7,
     gender:"female",
+    service: "Haircut",
     breed:"Samoyed"
 }
 let pet2={
     name:"Cinnamoroll",
     age:5,
     gender:"Male",
+    service: "Bath",
+    breed:"Pitbull"
+}
+let pet3={
+    name:"Chimera",
+    age:5,
+    gender:"Female",
+    service: "De-shedding",
     breed:"Pitbull"
 }
 
-document.write(pet1.name);
-document.write(pet2.name);
+
 
 //push pets into the array
 
-pets.push(pet1,pet2);
+pets.push(pet1,pet2,pet3);
 
+console.log("Total Pets = "+pets.length);
 
 function displayNames(){
     
-console.log("Total Pets = "+pets.length);
-
-console.log(pets[0].name);
-console.log(pets[1].name);
+    for(counter=0;counter<pets.length;counter++){
+        document.getElementById("PetRegistry").innerHTML+=`<div>
+        
+        <p>${pets[counter].name}</p>
+        
+        </div>`
+    }
 }
 
 displayNames();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
